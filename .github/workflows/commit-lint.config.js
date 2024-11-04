b851153674043c5723f8217d98e5d30aa0d592bd
Nhi TRAN <ntran@ntdt.fr>
2024-11-04 11:42:42 +0100

    chore(workflow): Fix commitlint workflow

--- .github/workflows/commit-lint.config.js
+++ .github/workflows/commit-lint.config.js
@@ -7,4 +7,11 @@
 // Here’s a simple configuration for Conventional Commits
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'type-enum': [2, 'always', ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test', 'perf']],
+        'scope-empty': [2, 'never'],         // Require a scope
+        'subject-case': [2, 'always', 'sentence-case'],
+        'subject-min-length': [2, 'always', 10],
+        'subject-max-length': [2, 'always', 50],
+      },
 };
